e43bcbc0a3d43073fee39f80fc20e249f06eaa1d
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2021-11-08 12:30:59 -0500

    Update scripts.js
    created "pokemonList" Variable

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,23 @@
-alert('Hello world');
-
-let favoriteFood = "Chicken Alfredo"
-document.write(favoriteFood);
+let pokemonList= [
+    {
+      name: 'Charizard',
+      height: 1.7,
+      type: ['fire', 'flying']
+    },
+    {
+      name: 'Lugia',
+      height: 5.2,
+      type: ['Psychic', 'flying']
+    },
+    {
+      name: 'Ho-Oh',
+      height: 3.8,
+      type: ['Fire']
+    },
+    {
+      name: 'Mew',
+      height: 0.4,
+      type: ['Psychic']
+    },
+   
+  ];
